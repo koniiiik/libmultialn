05b010666512729588cbc6328e13eac15160e6f3
Michal Petrucha <michal.petrucha@ksp.sk>
2012-04-13 01:11:55 +0200

    Code cleanups to match chosen code style.

--- include/SequenceDetails.h
+++ include/SequenceDetails.h
@@ -9,22 +9,13 @@
 
 class SequenceDetails
 {
-    private:
-        // position in the source sequence, size of this subsequence and
-        // the size of the original sequence
-        size_t start, size, src_size;
-        // true if from reverse-complement source
-        bool reverse;
-        std::shared_ptr<cds_static::BitSequence> sequence;
-
-
     public:
-        SequenceDetails(size_t _start, size_t _size, bool _reverse,
-                        size_t _src_size,
-                        cds_static::BitSequence * bseq):
-            start(_start), size(_size), reverse(_reverse),
-            src_size(_src_size), sequence(bseq)
-            {};
+        SequenceDetails(size_t start, size_t size, bool reverse,
+                        size_t src_size,
+                        cds_static::BitSequence *sequence):
+            start_(start), size_(size), reverse_(reverse),
+            src_size_(src_size), sequence_(sequence)
+        { }
 
         /*
         ** Given a position on the whole sequence returns the position in
@@ -48,6 +39,20 @@ class SequenceDetails
 
         size_t rank(size_t index) const;
         size_t select(size_t index) const;
+
+        size_t get_start() const
+        {
+            return this->start_;
+        }
+
+
+    private:
+        // position in the source sequence, size of this subsequence and
+        // the size of the original sequence
+        size_t start_, size_, src_size_;
+        // true if from reverse-complement source
+        bool reverse_;
+        std::shared_ptr<cds_static::BitSequence> sequence_;
 };
 
 #endif /* SEQUENCEDETAILS_H */
--- src/SequenceDetails.cpp
+++ src/SequenceDetails.cpp
@@ -5,35 +5,35 @@
 
 size_t SequenceDetails::sequenceToAlignment(size_t index) const
 {
-    return this->sequence->select1(index - this->start + 1);
+    return this->sequence_->select1(index - this->start_ + 1);
 }
 
 size_t SequenceDetails::alignmentToSequenceBegin(size_t index) const
 {
     size_t next1 = index;
-    if (!this->sequence->access(index))
+    if (!this->sequence_->access(index))
     {
-        next1 = this->sequence->selectNext1(index);
+        next1 = this->sequence_->selectNext1(index);
     }
-    return this->sequence->rank1(next1) + this->start - 1;
+    return this->sequence_->rank1(next1) + this->start_ - 1;
 }
 
 size_t SequenceDetails::alignmentToSequenceEnd(size_t index) const
 {
     size_t prev1 = index;
-    if (!this->sequence->access(index))
+    if (!this->sequence_->access(index))
     {
-        prev1 = this->sequence->selectPrev1(index);
+        prev1 = this->sequence_->selectPrev1(index);
     }
-    return this->sequence->rank1(prev1) + this->start - 1;
+    return this->sequence_->rank1(prev1) + this->start_ - 1;
 }
 
 size_t SequenceDetails::rank(size_t index) const
 {
-    return this->sequence->rank1(index);
+    return this->sequence_->rank1(index);
 }
 
 size_t SequenceDetails::select(size_t index) const
 {
-    return this->sequence->select1(index);
+    return this->sequence_->select1(index);
 }

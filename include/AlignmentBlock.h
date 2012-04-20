c255f8e7f2146f3e5dd2cdf959ee109d61fbd819
Michal Petrucha <michal.petrucha@ksp.sk>
2012-04-21 01:07:38 +0200

    Added a description of the AlignmentBlock interface.

--- include/AlignmentBlock.h
+++ include/AlignmentBlock.h
@@ -8,6 +8,7 @@
 #include <SequenceDetails.h>
 #include <MultialnConstants.h>
 
+
 class SequenceDoesNotExist: public std::exception
 {
     public:
@@ -34,13 +35,35 @@ class AlignmentBlock
             this->copySequences(other.sequences_);
         }
 
+        /*
+        ** Takes a position in the reference sequence and maps it to a
+        ** single specified informant.
+        **
+        ** Throws OutOfSequence in case the position can't be mapped
+        ** within this block.
+        */
         const size_t mapPositionToInformant(const size_t pos,
                 const std::string &informant,
                 const IntervalBoundary boundary=INTERVAL_BEGIN) const;
+        /*
+        ** Takes a position in the reference sequence and maps it to those
+        ** informants where such mapping is possible.
+        **
+        ** Throws OutOfSequence in case the position is not present in the
+        ** reference sequence.
+        */
         const Mapping * mapPositionToAll(const size_t pos,
                 const IntervalBoundary boundary=INTERVAL_BEGIN) const;
 
+        /*
+        ** Returns the named sequence. Throws SequenceDoesNotExist if not
+        ** present.
+        */
         const SequenceDetails * getSequence(const std::string &name) const;
+        /*
+        ** Returns the reference sequence. Throws SequenceDoesNotExist if
+        ** the sequence has not yet been added to this block.
+        */
         const SequenceDetails * getReferenceSequence() const
         {
             return this->getSequence(*(this->reference_name_));
